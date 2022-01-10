d63c1da6a079a6ab3a0042697f8127a9a59bf031
toast23 <tyao2005@gmail.com>
2022-01-10 14:27:52 -0500

    snow falls and arrow keys control speed

--- Sketch.java
+++ Sketch.java
@@ -3,34 +3,73 @@ import processing.core.PApplet;
 public class Sketch extends PApplet {
 	
 	
+  // global variables 
+  public float width = 400f;
+  public float height = 800f;
+  public float circleX;
+  public float speed = 5f;
+  public float snowSize = 7.5f;
+
+  public float[] circleY = new float[25];
+  public float[] snowPile = new float[(int)width];
+  
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size((int)width, (int)height);
   }
+
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
   }
+
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    background(50);
+  //  rectRotated(20);
+    if (keyPressed) {
+      if (keyCode == UP) {
+        speed = 5;
+      }
+      else if (keyCode == DOWN) {
+        speed = 0.75f;
+      }
+    }
+    else {
+      speed = 2;
+    }
+    for (int i = 0; i < circleY.length; i++) {
+      circleX = width * i / circleY.length;
+      ellipse(circleX, circleY[i], snowSize, snowSize);
+      circleY[i]+=speed;
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+    }
   }
   
-  // define other methods down here.
+
+  public void rectRotated(float rectHeight) {
+    pushMatrix();
+    fill(255);
+    translate(200, height);
+    rotate(TWO_PI / 2);
+    rect(0, 0, 10, rectHeight);
+    popMatrix();
+  }
+
+
 }
