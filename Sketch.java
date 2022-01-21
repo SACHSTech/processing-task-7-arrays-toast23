fd803f9f8eae532de5377a31a4d5f2120411b38b
toast23 <tyao2005@gmail.com>
2022-01-20 23:07:52 -0500

    finished task 7

--- Sketch.java
+++ Sketch.java
@@ -1,17 +1,29 @@
 import processing.core.PApplet;
+import java.util.Random;
+
 
 public class Sketch extends PApplet {
 	
 	
   // global variables 
-  public float width = 400f;
+  Random r = new Random();
+
+  public float width = 700f;
   public float height = 800f;
   public float circleX;
   public float speed = 5f;
   public float snowSize = 7.5f;
 
-  public float[] circleY = new float[25];
-  public float[] snowPile = new float[(int)width];
+  public int heightScale = (int)height/800;
+  public int num = 40;
+  public int max = 170*heightScale;
+  public int min = 165*heightScale;
+
+  public float[] circleY = new float[(int)width/7];
+  public float[] snowPile = new float[(int)width/7];
+
+  public int[] pMouseX = new int[num];
+  public int[] pMouseY = new int[num];
   
 
   /**
@@ -31,6 +43,9 @@ public class Sketch extends PApplet {
     for (int i = 0; i < circleY.length; i++) {
       circleY[i] = random(height);
     }
+    for (int i = 0; i < snowPile.length; i++) {
+      snowPile[i] = r.nextInt((max - min) + 1) + min;
+    }
   }
 
 
@@ -38,8 +53,17 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    background(50);
-  //  rectRotated(20);
+    background(0);
+    speedChange();
+    snowFall();
+    snowPile();
+    snowTrail();
+ 
+  }
+  
+  
+  // arrows to change speed
+  public void speedChange() {
     if (keyPressed) {
       if (keyCode == UP) {
         speed = 5;
@@ -51,25 +75,55 @@ public class Sketch extends PApplet {
     else {
       speed = 2;
     }
+  }
+
+
+  // snow fall
+  public void snowFall() {
     for (int i = 0; i < circleY.length; i++) {
       circleX = width * i / circleY.length;
       ellipse(circleX, circleY[i], snowSize, snowSize);
       circleY[i]+=speed;
       if (circleY[i] > height) {
         circleY[i] = 0;
+        snowPile[i]+=2;
       }
-    }
+     }
   }
-  
 
-  public void rectRotated(float rectHeight) {
+
+  // orientate snowpile 90 degrees cw
+  public void rectRotated(float x, float y, float snowWidth, float snowHeight) {
     pushMatrix();
-    fill(255);
-    translate(200, height);
-    rotate(TWO_PI / 2);
-    rect(0, 0, 10, rectHeight);
+    fill(230);
+    translate(0, height);
+    rotate(TWO_PI / 0.25f);
+    rect(x, y, snowWidth, snowHeight);
     popMatrix();
   }
 
+
+  // build snow pile
+  public void snowPile() {
+    for (int i = 0; i < snowPile.length; i++) {  
+      rectRotated(i*7, 0, 5, -1*snowPile[i]);
+    }
+  }
+
+
+  // snow trails cursor
+  public void snowTrail() {
+    stroke(245);
+    for (int i = num-1; i > 0; i--) {
+      pMouseX[i] = pMouseX[i-1];
+      pMouseY[i] = pMouseY[i-1];
+    }
+    pMouseX[0] = mouseX;
+    pMouseY[0] = mouseY;
+    for (int i = 0; i < num; i++) {
+      ellipse(pMouseX[i], pMouseY[i], num-i/2.0f, num-i/2.0f);
+    }
+  }
+ 
 
 }
